1043e0e64361b54436023c86c4098edf1d750001
Yanu <59870900+khusanova@users.noreply.github.com>
2026-09-22 13:56:41 -0400

    Refactor server.py after running static code analysis

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request 
+"""Server for Emotion Detection Flask application"""
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 
@@ -7,6 +8,12 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def get_emotion_detector():
+    """
+    Get result of emotion detection with Watson NLP Library.
+
+    Returns:
+        Formatted text with the result of emotion detection in the text provided in request.
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
     # Pass the text to the emotion_detector function and store the response
@@ -20,21 +27,26 @@ def get_emotion_detector():
     output = "For the given statement, the system response is "
     for i in range(5):
         if i == 3:
-            output += "'{}': {} and ".format(labels[i], scores[i])
+            output += f"'{labels[i]}': {scores[i]} and "
         elif i == 4:
-            output += "'{}': {}. ".format(labels[i], scores[i])
+            output += f"'{labels[i]}': {scores[i]}. "
         else:
-            output += "'{}': {}, ".format(labels[i], scores[i])
+            output += f"'{labels[i]}': {scores[i]}, "
 
-    output += "The dominant emotion is {}.".format(response["dominant_emotion"])
+    output += f"The dominant emotion is {response['dominant_emotion']}."
     return output
 
 
-  
 @app.route("/")
 def render_index_page():
+    """
+    Render the main page for the web application.
+
+    Returns:
+        Rendered template for the main page.
+    """
     return render_template('index.html')
 
 
 if __name__ == "__main__":
-      app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
